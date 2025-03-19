b6b256e77e83c84346f3a366c6f68c91c1cd5034
Khaled Oghli <khalidoghli7@gmail.com>
2025-03-19 11:49:44 +0400

    fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
         {/* <MapView /> */}
 
         <ModeToggle />
-        <h1 className='text-center text-4xl font-bold sm:text-left'>Welcome to Next Bilorplate </h1>
+        <h1 className='text-center text-4xl font-bold sm:text-left'>
+          Welcome to Next Boilerplate{' '}
+        </h1>
       </main>
       <footer className='row-start-3 flex flex-wrap items-center justify-center gap-[24px]'>
         <a
